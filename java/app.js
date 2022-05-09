1b46689570ca928769a9c7e6881cdc4e9a9324d7
Senad Djerlek <senajerlek@gmail.com>
2022-05-09 18:46:51 +0200

    javascript

---
+++ java/app.js
@@ -0,0 +1,63 @@
+
+// const ime="max"
+
+// let godine = 30;
+// console.log (ime + "" + godine);
+
+// godine= 25;
+// console.log (ime + "" + godine);
+
+// console.log(5 === 5);
+// console.log( 6 >= 5);
+// const ime="Max";
+// const godine = 40;
+// const meseci = godine * 12;
+
+
+// console.log(meseci);
+// const nesto =prompt ("Unesi tvoje ime");
+// const godine =prompt("Unesi tvoje godine");
+// alert("Ziveo si " + godine * 12 + "meseci");
+
+// let age = 20;
+// console.log (age++);    
+// console.log(age);
+// age= age-1;
+
+// const age = prompt("unesi tvoje godine");
+// if (+age === 18) {
+//     console.log("Tek si postao punoletan");
+// }
+// else if (age> 18){
+//     console.log("Ti si odrastao");
+// }
+// else if (age > 12){
+//     console.log ("Ti si tinejdzer");
+// }
+// else{
+//     console.log("Ti si dete");
+// }
+// // if (age > 18){
+//     console.log ("Punoletan si ");
+// }
+// else if (age < 18) {
+//     console.log("Nisi punoletan")
+
+// }
+// else {
+//     console.log ("Izvrsava se samo u slucaju kada ni jedan uslov nije zadovoljen")
+// }
+// const a = 4;
+// const b = 5;
+// const c = 0;
+
+// console.log ( a && b && c); -- and
+
+// const a = 4;
+// const b = 10;
+
+// console.log (a || b); - or
+
+// console.log(!false); 
+
+

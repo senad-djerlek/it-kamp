01fbf91bc82b3577e872f715560f1adb9ae7205e
Senad Djerlek <senajerlek@gmail.com>
2022-05-30 18:50:24 +0200

    javascript array metode

--- java/app.js
+++ java/app.js
@@ -1,4 +1,3 @@
-
 // const ime="max"
 
 // let godine = 30;
@@ -13,14 +12,13 @@
 // const godine = 40;
 // const meseci = godine * 12;
 
-
 // console.log(meseci);
 // const nesto =prompt ("Unesi tvoje ime");
 // const godine =prompt("Unesi tvoje godine");
 // alert("Ziveo si " + godine * 12 + "meseci");
 
 // let age = 20;
-// console.log (age++);    
+// console.log (age++);
 // console.log(age);
 // age= age-1;
 // console.log(age)
@@ -48,18 +46,18 @@
 // else {
 //     console.log ("Izvrsava se samo u slucaju kada ni jedan uslov nije zadovoljen")
 // }
-    // const a = 4;
-    // const b = 5;
-    // const c = 0;
+// const a = 4;
+// const b = 5;
+// const c = 0;
 
-    // console.log ( a && b && c); -- and
+// console.log ( a && b && c); -- and
 
 // const a = 4;
 // const b = 10;
 
 // console.log (a || b); - or
 
-// console.log(!false); 
+// console.log(!false);
 // const age = prompt("unesi tvoje godine");
 
 // if (+age >= 18 || +age < 40) {
@@ -109,12 +107,12 @@
 //         n=21;
 //  }
 
- //FIZZ BUZZ CHALLENGE
+//FIZZ BUZZ CHALLENGE
 
- // IDEMO OD BROJEVA OD 0 DO 100 I ISPISUJEMO TE BROJEVE;
- //AKO JE BROJ DELJIV SA 3 ISPISUJEMO FIZZ
- //AKO JE BROJ DELJIV SA 5 ISPISUJETE BUZZ
- // AKO JE BROJ DELJIV SA 3 I SA 5 ISPISUJEMO FIZZ BUZZ
+// IDEMO OD BROJEVA OD 0 DO 100 I ISPISUJEMO TE BROJEVE;
+//AKO JE BROJ DELJIV SA 3 ISPISUJEMO FIZZ
+//AKO JE BROJ DELJIV SA 5 ISPISUJETE BUZZ
+// AKO JE BROJ DELJIV SA 3 I SA 5 ISPISUJEMO FIZZ BUZZ
 // const rec= "Sta ste radili danas"
 // let counter = 0;
 // for (let i = 0; i< rec.length; i++){
@@ -132,13 +130,13 @@
 
 //     if (i === 0 ) {
 //         newString += element.toUpperCase();
-        
+
 //     } else if (string [i-1] === ""){
 //         newString += element.toUpperCase();
 //     } else {
 //         newString += element;
 //     }
-    
+
 // }
 
 // console.log(newString);
@@ -149,7 +147,6 @@
 // for (let i=0; i< string.length; i++ ){
 
 // }
-
 
 // for (let i = 0; i <100; i++) {
 //     if ( i % 3 === 0 && i % 5 === 0)
@@ -195,16 +192,12 @@
 //         console.log("nemoj da nosis naocare");
 //     }
 
-
 //TERNARY OPERATOR
-
-
 
 //mdn web docs
 
-    // if(isSunny) // if(isSunny === true)
-    // if (!isSunny) // if(isSunny !== true) || if(isSunny === false)
-
+// if(isSunny) // if(isSunny === true)
+// if (!isSunny) // if(isSunny !== true) || if(isSunny === false)
 
 // if (""){
 //     console.log("Tacna vrednost ili thruthy")
@@ -216,7 +209,6 @@
 
 // const niz = ["BMW",4,undefined ,"VW",null, NaN, false,true, "sadas","kurs"  ]
 
-
 // for (let clan in niz){
 //     console.log("Index",clan, "Vrednost clana",niz[clan]);
 // }
@@ -227,21 +219,13 @@
 
 // console.log(studenti);
 
-
-
-
-
-
-
 // for (let i=0; i<cars.length; i++){
 //     if (cars [i]){
 //         console.log(cars[i]);
 //     }
-    
 
-                //0 , 1,    2             3,         4
+//0 , 1,    2             3,         4
 // console.log(cars[1],cars[3]);
-
 
 // const fruits = ["Banana","Appple","Pear"];
 // fruits [2] = 2;
@@ -258,7 +242,7 @@
 //     } else {
 //         console.log("not true")
 //     }
-    
+
 // }
 
 // const cars = ["Audi", "BMW","Mercedes"];
@@ -270,11 +254,10 @@
 //     }
 // }
 
-
 // function saberi_tri_pet(){
 //     const zbir = 3 + 5;
 //     return zbir;
-    
+
 // }
 
 // function saberi(prviBroj,drugiBroj){
@@ -296,21 +279,17 @@
 
 // console.log(saberi_tri_pet());
 
-
 // function inchToCm(inch){
 //     return inch * 2.54;
 // }
 
 // console.log(inchToCm(10))
 
-
 // const visina = inchToCm(25);
 
 // console.log(visina);
 
-
 // const inchToCm = inch =>  inch * 2.54;
-
 
 // console.log(inchToCm(10));
 
@@ -324,7 +303,7 @@
 //     const name = "Jusuf";
 // }
 
-// function sabiraj  (prvi, drugi, treci = false) 
+// function sabiraj  (prvi, drugi, treci = false)
 // {
 //     if(treci) {
 //         return prvi + drugi + treci;
@@ -343,16 +322,12 @@
 // let brojTri = prompt("Unesi treci broj")
 // console.log(brojTri)
 
-
 // function mnozenjeUnetihBrojeva (broj,brojDva,brojTri) {
 //     const mnozenje = broj * brojDva * brojTri
 //     return mnozenje;
 // }
 
 // alert(mnozenjeUnetihBrojeva(broj , brojDva, brojTri));
-
-
-
 
 // alert(sabiranjeUnetihBrojeva(broj,brojDva,brojTri));
 
@@ -370,47 +345,41 @@
 
 //ZA DODAVANJE NA POCETAK NIZA - unshift
 
-const cars = ["Bmw","Audi","Smart"];
-cars.push("Mercedes","Fiat,Mini")
+// const cars = ["Bmw","Audi","Smart"];
+// cars.push("Mercedes","Fiat,Mini")
 
-cars.unshift("Ford","Kadilak", 4, "Nesto");
+// cars.unshift("Ford","Kadilak", 4, "Nesto");
 
+// //ZA BRISANJE PRVOG CLANA NIZA - shift
+// cars.shift();
 
+// const shiftValue = cars.shift();
+// console.log(shiftValue);
 
-//ZA BRISANJE PRVOG CLANA NIZA - shift
-cars.shift();
+// console.log(cars);
 
-const shiftValue = cars.shift();
-console.log(shiftValue);
+// //OBRNI NIZ - reverse
 
+// cars.reverse();
+// console.log(cars);
 
-console.log(cars);
+// //SPAJANJE DVA NIZA - concat(niz);
 
+// const imena = ["faris" , "isko", "aldinn"]
+// const age = [20,100,50];
 
-//OBRNI NIZ - reverse
+// const spojenNiz = imena.concat(age);
+// console.log(spojenNiz);
 
-cars.reverse();
-console.log(cars);
+// //TRAZENJE CLANA NIZA - find();
 
-//SPAJANJE DVA NIZA - concat(niz);
+// const trazenaVrednost = cars.find((el) => console.log(el) );
+// console.log(trazenaVrednost);
 
-const imena = ["faris" , "isko", "aldinn"]
-const age = [20,100,50];
+// //TRAZENJE INDEKSA - find indeks trazeni indeks
 
-const spojenNiz = imena.concat(age);
-console.log(spojenNiz);
-
-//TRAZENJE CLANA NIZA - find();
-
-const trazenaVrednost = cars.find((el) => console.log(el) );
-console.log(trazenaVrednost);
-
-
-//TRAZENJE INDEKSA - find indeks trazeni indeks
-
-const trazeniIndex = age.findIndex(el => el <100);
-console.log(trazeniIndex);
-
+// const trazeniIndex = age.findIndex(el => el <100);
+// console.log(trazeniIndex);
 
 // const godine = [12 , 76, 26, 6 , 23 , 21 ,29 ,16];
 // const drugeGodine =[12,76,8,10,5,14,28,98,101]
@@ -425,7 +394,7 @@ console.log(trazeniIndex);
 // }
 // // function returnAdults(ages) {
 // //     const adults = [];
-// //     for (const age of ages) { 
+// //     for (const age of ages) {
 // //         if (age >= 18) {
 // //             adults.push(age)
 // //         }
@@ -438,4 +407,204 @@ console.log(trazeniIndex);
 
 //
 
+// const DATA1 = [2,26,38,75,11,29]
+// const DATA2 = ["a","b","c","d","e"]
+
+// const mutateArrays = {array1,array2} =>{
+
+// }
+
+// const mutateArrays = (array1,array2) => {
+// }
+// function(dat1,dat2){
+//     let helper= dat1.concat(dat2);
+//     let parni =[];
+
+//     for (const el of helper){
+//         if (el % 2 === 0){
+//             parni.push(el);
+//         }
+//     }
+//     parni.unshift(10,20)
+//     return helper,parni;
+// }
+// console.log(mutateArrays(DATA1,DATA2));
+//prvi jeo je da napravimo pomocni niz koji ce da sadrzi dva niza spojena
+//iz tog pomocnog niza sto smo napravili napravimo koji ce da izvuce iz ovog niza samo parne brojeve
+// na prva dva mesta unesi broj 10 i broj 20
+
+//FILTER METODA
+// const words = ["televizor","daljinski","telefon","voda","ranac","stolica"];
+// const numbers = [2,8,9,11,26,85,86,90,102,1003,1120];
+// const booleans = [true,false,true,false]
+// const filteredArray = words.concat(numbers).concat(booleans).filter(el => typeof el === "boolean");
+
+// console.log(filteredArray);
+
+//Map metoda
+
+// const numbers = [25, 100, 9, 16, 84];
+
+// const squareNumbers = numbers.map((el, index, aray) => {
+//   if (index === 0 || index === 3 || index === 5) {
+//     return el * el;
+//   } else{
+//     return el;
+//   }
+// });
+// console.log(squareNumbers);
+
+// const DATA1 = [1, 3, 4, 2, 7, 14];
+
+// const dogAgeToHuman = (ages) => {
+// const humanAge = ages.map(age => {
+//     if (age >2 && age < 9){
+//         return  (age * 4) / 2;
+//     }else if (age > 2){
+//         return age * 7;
+//     }
+//     else {
+//         return age;
+//     }
+// }); return humanAge;
+//     const lessThan10HumanAge = humanAge.filter(el => el < 10);
+//     return lessThan10HumanAge;
+// }
+// console.log(dogAgeToHuman(DATA1));
+
+// const DATA1 = [1, 3, 4, 2, 7, 14];
+
+//  const dogAgeToHuman = (ages) => {
+//     const humanAge = ages.map(age =>
+//         {
+//             if (age > 2 && age < 9){
+//                 return (age * 4) / 2;
+//             } else if (age > 2){
+//                 return age * 7;
+//             }
+//             else {
+//                 return age;
+//             }
+
+//         })
+//  }
+//  console.log(dogAgeToHuman(DATA1))
+
+// const DATA1 = [1, 3, 4, 2, 7, 14];
+
+// const dogAgeToHuman = (ages) => {
+//     const humanAge = ages.map(age =>{
+//         if (age >= 10){
+//             return (age * 4 );
+//         }else if (age > 10){
+//             return age *25;
+//         }else {
+//             return age;
+//         }
+//     })
+
+// }
+
+// console.log(dogAgeToHuman(DATA1));
+
+// const DATA1 = [10,12,20,50,25];
+
+// const BrojeviVeciOd10 = DATA1.map(el => el * 10);
+
+// console.log(BrojeviVeciOd10);
+
+// const DATA1 = [10,12,20,50,25];
+
+// const BrojeviVeciOd10 = (brojevi) => {
+//     const ispisBrojeva = brojevi.map(broj => {
+//         if (broj > 10){
+//             return brojevi;
+//         }
+//     })
+
+// }
+
+// console.log(BrojeviVeciOd10(DATA1));
+
+//MAP
+// const DATA = [100, 65, 76, 80, 99, 53, 1,2];
+
+// const returnEven = (numbers) => {
+// return (evenNumbers = numbers.filter(num => num % 2 === 0 && num <= 50));
+
+// };
+
+// const DATA = [5,3,2,8,7,9];
+
+// const returnPow = (numbers) => {
+// return pow = numbers.map(el => el * el + 10)
+
+// };
+
+// reduce funkcija
+
+// const DATA = [100,200,250,400,520];
+
+// const cartTotal = DATA.reduce(
+// (previousValue, currentValue) => previousValue + currentValue,
+// 500
+// );
+
+const DATA = [26, 18, 16, 22, 30, 28];
+
+const sumAvgAge = (ages) => {
+
+    const getAverage =
+    ages.reduce((prevValue, curValue) => prevValue + curValue) / ages.length;
+
+    return +getAverage.toFixed(2);
+
+// return average = zbirSvihGodina / ages.length;
+
+
+
+
+
+}
+
+console.log(sumAvgAge(DATA));
+
+// // every
+// const ages = [32, 33, 16, 40];
+
+// ages.every(checkAge)
+
+// function checkAge(age) {
+//   return age > 18;
+// }
+
+
+//some 
+
+// const ages = [3, 10, 18, 20];
+
+// ages.some(checkAdult);
+// function checkAdult(age) {
+//   return age > 18;
+// }
+
+//includes
+
+const array1 = [ 1 , 2 , 3];
+
+console.log(array1.includes(2));
+
+
+const pets = ["cat ", "dog", "bat"];
+
+console.log(pets.includes("dog"));
+
+//join
+
+const elements = ["fire","air","water", "trece"];
+
+console.log(elements.join());
+// expected output : Fire,Air,Water
+
+console.log(elements.join(" "));
 
